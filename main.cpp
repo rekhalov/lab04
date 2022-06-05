1c1feba28e3a610406d488c1be064f10c1804dd8
rekhalov <rekhalovas@yandex.ru>
2022-06-05 19:59:52 +0300

    CURL *

--- main.cpp
+++ main.cpp
@@ -13,26 +13,33 @@ vector<double> input_numbers(istream &in, size_t count) {
     }
     return result;
 }
-Input read_input(istream& in) {
+Input read_input(istream& in, bool prompt) {
     Input data;
-    cerr << "Enter number count: ";
+    if (prompt) cerr << "Enter number count: ";
     size_t number_count;
     in >> number_count;
-    cerr << "Enter numbers: ";
+    if (prompt) cerr << "Enter numbers: ";
     data.numbers = input_numbers(in, number_count);
-    cerr << "Enter bin count: ";
+    if (prompt) cerr << "Enter bin count: ";
     in >> data.bin_count;
     return data;
 }
 
 
 int main(int argc, char* argv[]) {
+    curl_global_init(CURL_GLOBAL_ALL);
     if (argc > 1) {
-        printf("%d %s", argc, *argv);
+         CURL *curl = curl_easy_init();
+        if(curl) {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+        }
         return 0;
     }
     curl_global_init(CURL_GLOBAL_ALL);
-    const auto data = read_input(cin);
+    const auto data = read_input(cin, true);
     const auto bins = make_histogram(data);
     show_histogram_svg(bins);
     return 0;
